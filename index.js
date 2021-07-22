c74cb66b5b99792466c348e93f0ec31cd0c09b92
AniElaina <anicook13@gmail.com>
2021-07-22 18:34:37 -0400

    began new round of questions for team generator

--- index.js
+++ index.js
@@ -61,7 +61,62 @@ var questionsPrompt = () => {
         .then(managerInfo) = function () {
             let { nameinput, idInput, emailInput, officeNumberInput } = managerInfo;
             let manager = new Manager /// need to create Manager class first (in lib/Employee.js??)
+            /// push manager into team array??
         }
 };
 
-// questionsPrompt();
+/// add function to present user "menu" of options to add either intern or engineer to the team 
+// these question prompts need to be wrapped in above mentioned function 
+return inquirer.prompt([
+    {
+        type: "confirm",
+        name: "addMore",
+        message: "Would you like to add more team members?"
+        // if yes, continue. if no, return. how to set up if/else statement here without causing bug?
+
+    },
+    {
+        type: "checkbox",
+        name: "role",
+        message: "Please select the role of the employee you are adding",
+        choices: ["Intern", "Engineer"]
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter the name of this employee (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Please enter the ID of the employee (Required)",
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your employee's email address (Required)",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("You must enter your manager's email address.")
+            }
+        }
+    }
+
+])
