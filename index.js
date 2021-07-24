99de85031a4ba56c15b405983dd5d15e3c55facb
AniElaina <anicook13@gmail.com>
2021-07-24 15:56:52 -0400

    saving changes

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const fs = require("fs");
+
 const inquirer = require("inquirer");
 // don't forget to add a const variable here that requires file name we will write to
+const generatePage = require("./src/page-template");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+// const Employee = require("./lib/Employee");
+
 
 const myTeam = [];
 
@@ -58,65 +65,294 @@ var questionsPrompt = () => {
             }
         }
     ])
-        .then(managerInfo) = function () {
-            let { nameinput, idInput, emailInput, officeNumberInput } = managerInfo;
-            // let manager = new Manager /// need to create Manager class first (in lib/Employee.js??)
-            /// push manager into team array??
-        }
+        .then(managerInfo => {
+
+
+            let manager = new Manager(managerInfo.name, managerInfo.id, managerInfo.email, managerInfo.officeNumber);
+            myTeam.push(manager);
+            // console.log(myTeam);
+            console.log(myTeam);
+
+            nextQuestions();
+
+        })
+
+
 };
+function nextQuestions() {
+    inquirer.prompt([
+        {
+            name: "role",
+            type: "checkbox",
+            message: "Please select the role of the employee you're adding.",
+            choices: ["Intern", "Engineer"]
+
+
+
+
+
+        },
+        {
+            name: "github",
+            type: "input",
+            message: "Please enter github of this Engineer (Required)",
+            when: (answers) => answers.role[0] == "Engineer",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("You must enter the employee's Github username")
+                }
+            }
+        },
+        // {
+        //     type: "checkbox",
+        //     name: "role",
+        //     message: "Please select the role of the employee you are adding",
+        //     choices: ["Intern", "Engineer"]
+        // },
+
+        {
+            /// use inquirer boolean method "when"?? To make sure this is only asked if user selects engineer? same with intern's "school"
+
+
+            type: "input",
+            name: "github",
+            message: "Please enter the engineer's Github username (Required)",
+            when: (data) => data.role === "Engineer",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("You must enter the employee's Github username")
+                }
+            }
+
+
+        },
+
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter the name of this employee (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter the ID of the employee (Required)",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your employee's email address (Required)",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log("You must enter your employee's email address.")
+                }
+            }
+        },
+
+        {
+
+
+            type: "input",
+            name: "school",
+            message: "Please enter this intern's school (Required)",
+            when: (answers) => answers.role[0] === "Intern",
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                    console.log("You must enter the intern's school.");
+                }
+            }
+        },
+        {
+            type: "confirm",
+            name: "addMore",
+            message: "Would you like to continue adding team members? (Required)",
+            /// how to do if/else statement that will loop user back through menu? 
+            validate: addmoreConfirm => {
+                if (addmoreConfirm === true) {
+                    return true;
+                } else {
+                    console.log("Alright! Your team is looking great!");
+                }
+            }
+        }
+    ])
+        .then(newEmployeeInfo => {
+            // let employee = new Employee(newEmployeeInfo.name, newEmployeeInfo.id, newEmployeeInfo.email, newEmployeeInfo.githubInput);
+            // myTeam.push(employee);
+
+            // console.log(myTeam);
+
+            // let intern = new Intern(newEmployeeInfo.role[0], newEmployeeInfo.name, newEmployeeInfo.id, newEmployeeInfo.email, newEmployeeInfo.school);
+
+
+
+
+            if (newEmployeeInfo.role[0] === "Intern") {
+                let intern = new Intern(newEmployeeInfo.name, newEmployeeInfo.id, newEmployeeInfo.email, newEmployeeInfo.school);
+                console.log(intern);
+            } else if (newEmployeeInfo.role[0] === "Engineer") {
+                let engineer = new Engineer(newEmployeeInfo.name, newEmployeeInfo.id, newEmployeeInfo.email, newEmployeeInfo.github);
+                console.log(engineer);
+            }
+            myTeam.push(intern);
+            myTeam.push(engineer);
+            console.log(myTeam);
+
+            // if (addMore) {
+            //     return nextQuestions();
+            // } else {
+            //     return myTeam;
+            // }
+
+
+
+
+
+            // nextQuestions();
+
+        })
+
+}
+
+
+// questionsPrompt();
+
+function writeToFile(filename, data) {
+    fs.writeFile(filename, data, error => {
+        if (error) {
+            return console.log(error);
+        } else {
+            console.log("Your team has been been assembled!! Check out dist/index.html to see the results!");
+        }
+    })
+};
+
+function init() {
+    questionsPrompt()
+        .then(userAnswers => {
+            let samplePage = generatePage(userAnswers);
+            console.log(samplePage);
+            writeToFile("./dist/index.html", samplePage);
+        })
+}
+
+
+init();
+
 
 /// add function to present user "menu" of options to add either intern or engineer to the team 
 // these question prompts need to be wrapped in above mentioned function 
-return inquirer.prompt([
-    {
-        type: "confirm",
-        name: "addMore",
-        message: "Would you like to add more team members?"
-        // if yes, continue. if no, return. how to set up if/else statement here without causing bug?
+// return inquirer.prompt([
 
-    },
-    {
-        type: "checkbox",
-        name: "role",
-        message: "Please select the role of the employee you are adding",
-        choices: ["Intern", "Engineer"]
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "Please enter the name of this employee (Required)",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+// const printPage = function (pageHTML) {
+//     fs.writeFile("./dist/index.html", pageHTML, (err) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("Your page is being generated! Check out dist/index.html to see your team!");
+//         }
+//     });
+// };
 
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Please enter the ID of the employee (Required)",
-        validate: idInput => {
-            if (idInput) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Please enter your employee's email address (Required)",
-        validate: emailInput => {
-            if (emailInput) {
-                return true;
-            } else {
-                console.log("You must enter your manager's email address.")
-            }
-        }
-    }
 
-])
+
+
+
+
+
+
+
+// const writeToPage = data => {
+//     fs.writeFile("./dist/index.html", data, err => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("Your team has been assembled! Check out dist/index.html to see!")
+//         }
+//     })
+// };
+
+// writeToPage();
+
+
+
+// questionsPrompt()
+//     .then(nextQuestions())
+//     .then(myTeam => {
+//         return generatePage(myTeam);
+//     })
+//     .then(pageHTML => {
+//         return printPage(pageHTML);
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     });
+
+
+
+
+
+
+
+
+
+
+
+
+// .then(myTeam => {
+    //     return generatePage(myTeam)
+    // })
+    // .then(samplePage => {
+    //     writeToPage(samplePage);
+
+
+    // })
+    // .catch(err => {
+    //     if (err) {
+    //         console.log(err);
+    //     }
+    // })
+
+
+
+
+
+
+
+// init();
+
+
+
+
+// const writeToPage = function (samplePage) {
+//     fs.writeFile("./dist/index.html", samplePage, (err) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("Your page has been successfully generated! Check out dist/index.html!");
+//         }
+//     });
+// }
+
--- src/page-template.js
+++ src/page-template.js
@@ -1,34 +1,129 @@
-`<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Profile Generator</title>
-</head>
 
-<body>
-<header class="header" id="header">
-<h1 class="title" id="title">Team Profile Generator</h1>
-<!----------------------------------------------------BEGIN START OF CARDS HERE------------------------------------------------------------------>
-<div class="content col-sm">
-    <div class="row">
-        <div class="containerTwo" id="reponse-container">
-            <h4 class="name">${name}</h6>
-                <h6 class="id">${id}</h6>
-                <h6 class="email">${email}</h6>
-                <h6 class="officeNumber">${officeNumber}</h6>
-                <h6 class="github">${github}</h6>
-                <h6 class="school" id="school">${school}</h6>
-        </div>
+//write html for each of 3 roles
+
+const makeManager = function (manager) {
+  return `
+<div class="card col-6 border-dark bg-light shadow profile top-buffer" style="width: 18rem" id="employee">
+<div class="card-header mb-3 text-light" style="background-color: rgb(255, 0, 85)">
+    <h4>${manager.name}</h4>
+    <h5>Manager</h5>
+  </div>
+  <div class="card-body">
+<ul class="list-group list-group-flush">
+  <li class="list-group-item p-2">ID: ${manager.id} </li>
+  <li class="list-group-item p-2">Email: ${manager.email}</li>
+  <li class="list-group-item p-2">Office Number: ${manager.officeNumber}</li>
+</ul>
+</div>
+</div>`;
+}
+
+// getRole = function (data) {
+//     const employee = data[0];
+//     const role = employee.getRole();
+//     if (role === "Manager") {
+//         const manager = makeManager(employee)
+
+//         myTeam.push(manager);
+//     }
+//     const allEmployees = myTeam.join('');
+//     const pageHtmlEl = genPage(allEmployees)
+//     return pageHtmlEl;
+// }
+const makeIntern = function () {
+  return `
+  <div class="card border-dark col-6 bg-light shadow profile top-buffer" style="width: 18rem" id="employee2" >
+    <div class="card-header mb-3 text-light" style="background-color: rgb(0, 189, 196);">
+      <h4>${intern.name}</h4>
+      <h5>Intern</h5>
     </div>
+    <div class="card-body">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item p-2">ID: ${intern.id}</li>
+        <li class="list-group-item p-2">Email: ${intern.email}</li>
+        <li class="list-group-item p-2">School: ${intern.school}</li>
+      </ul>
+    </div>
+  </div >`
+}
+
+const makeEngineer = function () {
+  return `
+  <div class="card border-dark col-6 bg-light shadow profile top-buffer" style="width: 18rem" id="employee3">
+  <div class="card-header mb-3 text-light" style="background-color: rgb(0, 255, 179);">
+      <h4>${engineer.name}</h4>
+      <h5>Engineer</h5>
+  </div>
+  <div class="card-body">
+      <ul class="list-group list-group-flush">
+          <li class="list-group-item p-2">ID: ${engineer.id}</li>
+          <li class="list-group-item p-2">Email: ${engineer.email}</li>
+          <li class="list-group-item p-2">Github: ${engineer.github}</li>
+      </ul>
+  </div>
+</div> `
+};
 
 
-</header>
-    
+getRole = (team) => {
+  let teamArray = [];
+  console.log(team);
 
 
-</body>
 
-</html>`
+  for (let i = 0; i < team.length; i++) {
+    const person = team[i];
+    const role = person.getRole();
+    console.log(role);
+
+
+    if (role === "Manager") {
+      const manager = makeManager(person);
+      teamArray.push(manager);
+    } else if (role === "Engineer") {
+      const engineer = makeEngineer(person);
+      teamArray.push(engineer);
+    } else if (role === "Intern") {
+      const intern = makeIntern(person);
+      teamArray.push(intern);
+    }
+  }
+  const allEmployees = teamArray.join("");
+  const pageHtmlEl = genPage(allEmployees);
+  return pageHtmlEl;
+};
+
+function genPage(pageHtmlEl) {
+  return `
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="./style.css" />
+    <title>Profile Generator</title>
+  </head>
+  <body>
+    <header>My Team</header>
+    <div class="container">
+        <div class="row" id="page">
+        ${pageHtmlEl}
+        </div>
+        </div>
+      </body>
+    </html>`
+
+}
+
+
+
+
+module.exports = getRole;
